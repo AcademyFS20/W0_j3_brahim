8232e22c6a4c5c7bd93a89d6851df24ffc90099f
belghiti-brahim <belghiti.brahim@pm.me>
2021-09-30 16:35:18 +0100

    initial_commit

---
+++ second_degree_equation.js
@@ -0,0 +1,25 @@
+function delta(a,b,c){
+}
+
+var x1,x2,x;
+var delta = Math.pow(b,2)-(4*a*c) 
+
+
+if (delta>0){
+    x1=(-b-Math.sqrt(delta))/(2*a)
+    x2=(-b+Math.sqrt(delta))/(2*a)
+    console.log (x1,x2)
+}
+else if (delta=0){
+    x=(-b-Math.sqrt(delta))/(2*a)
+    consol.log(x)
+}
+else (delta<0){ 
+    console.log(nosolution)
+}
+
+
+
+
+
+    
